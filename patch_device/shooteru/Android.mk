b7aeba92eb32f4e6a3cf1e8345f2deccab40c7fe
yanhao <yanhao@xiaomi.com>
2012-10-23 11:11:00 +0800

    add devices shooteru(HTC EVDO 3D G17)

---
+++ patch_device/shooteru/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+include $(MIUI_CLEAR)
+MIUI_PRODUCT := shooteru
+MIUI_KERNEL := $(LOCAL_PATH)/kernel
+
+MIUI_KERNEL_BASE := 0x48000000
+MIUI_KERNEL_PAGESIZE := 2048
+MIUI_KERNEL_CMDLINE := no_console_suspend=1
+
+MIUI_PRODUCT_ROOT := $(LOCAL_PATH)/root
+MIUI_DEVICE_CONFIG := $(LOCAL_PATH)/device.conf
+include $(MIUI_RECOVERY)
