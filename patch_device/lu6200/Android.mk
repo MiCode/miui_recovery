931dc53b249b42c3ad8c514f93f0db3439180f39
mochangming <mochangming@xiaomi.com>
2012-11-22 17:55:56 +0800

    fix cmdline of lu6200

--- patch_device/lu6200/Android.mk
+++ patch_device/lu6200/Android.mk
@@ -5,7 +5,7 @@ MIUI_KERNEL := $(LOCAL_PATH)/kernel
 
 MIUI_KERNEL_BASE := 0x40200000 --ramdiskaddr 0x41a00000
 MIUI_KERNEL_PAGESIZE := 2048
-MIUI_KERNEL_CMDLINE := vmalloc=450M,console=ttyDCC0,115200,n8 androidboot.hardware=qcom
+MIUI_KERNEL_CMDLINE := console=ttyDCC0,115200,n8 androidboot.hardware=i_lgu loglevel=1
 
 MIUI_PRODUCT_ROOT := $(LOCAL_PATH)/root
 MIUI_DEVICE_CONFIG := $(LOCAL_PATH)/device.conf
